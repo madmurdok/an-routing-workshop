dc7c83f611b3c1a0387d10b74a6d9d0157df4a27
Valentin Taiurskii <Valentin_Taiurskii@epam.com>
2018-10-05 18:02:22 +0300

    WIP pipes sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy  } from '@angular/core';
+import { MessagesService, SpinnerService  } from './core';
+import { Title, Meta } from '@angular/platform-browser';
+
+import { Router, NavigationEnd  } from '@angular/router';
+// rxjs
+import { Subscription } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
+
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent { }
+export class AppComponent implements OnInit, OnDestroy {
+  private sub: Subscription;
+
+  constructor(public messagesService: MessagesService,
+    private router: Router,
+    public spinnerService: SpinnerService,
+    private titleService: Title,
+    private metaService: Meta) { }
+
+  onActivate($event) {
+    console.log('Activated Component', $event);
+  }
+
+  onDeactivate($event) {
+   console.log('Deactivated Component', $event);
+  }
+
+  onDisplayMessages(): void {
+    this.router.navigate([{ outlets: { messages: ['messages'] } }]);
+    this.messagesService.isDisplayed = true;
+  }
+
+  ngOnInit() {
+    this.setPageTitlesAndMeta();
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  private setPageTitlesAndMeta() {
+    this.sub = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => this.router.routerState.root),
+        map(route => {
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        filter(route => route.outlet === 'primary'),
+        switchMap(route => route.data)
+      )
+      .subscribe(
+        data => {
+          this.titleService.setTitle(data['title']);
+          this.metaService.addTags(data['meta']);
+     }
+    );
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { LayoutModule } from './layout/layout.module';
+import { TasksModule } from './tasks/tasks.module';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,12 +16,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    SharedModule,
+    FormsModule,
+    LayoutModule,
+    TasksModule,
+    CoreModule,
+    // last palace
+    AppRoutingModule
   ],
   providers: [
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-
+  constructor(router: Router) {
+    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
 }
